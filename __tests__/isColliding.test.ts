be73a93b5a19678e8029e158a936b3ee4a1b5e7a
Petar Slovic <petar@petar.io>
2017-06-29 21:46:12 +0200

    add jest watch script

--- __tests__/isColliding.test.ts
+++ __tests__/isColliding.test.ts
@@ -42,4 +42,23 @@ describe('isColliding', () => {
 
     expect(result).toBe(true)
   })
+  })
+
+  it('should detect multiple agents NOT colliding', () => {
+    const agents = [{
+      position: 2
+    }, {
+      position: 11
+    }, {
+      position: 3
+    }, {
+      position: 22
+    }, {
+      position: 50
+    }]
+
+    const result = Boolean(isColiding(agents[0], agents))
+
+    expect(result).toBe(false)
+  })
 })
